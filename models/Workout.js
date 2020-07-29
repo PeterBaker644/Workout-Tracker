bf3d6e781d546ec2c735ab777812346bac645fc6
Blackstar <baker644@umn.edu>
2020-07-29 17:57:41 -0500

    Added working get method

--- models/Workout.js
+++ models/Workout.js
@@ -8,10 +8,11 @@ const WorkoutSchema = new Schema({
         {
             type: { type: String, required: true},
             name: { type: String, trim: true, required: true},
-            duration: Number,
+            duration: {type: Number, required: true},
             weight: Number,
             reps: Number,
-            sets: Number
+            sets: Number,
+            distance: Number
         }
     ]
 });
--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,39 +1,41 @@
-const db = require("../models");
+const Workout = require("../models/Workout");
 
 // Basic structure in place, go route by route and rework for workouts.
 
 module.exports = function (app) {
 
-    // app.post("/submit", ({ body }, res) => {
-    //     db.Book.create(body)
-    //         .then(({ _id }) => db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
-    //         .then(dbLibrary => {
-    //             res.json(dbLibrary);
-    //         })
-    //         .catch(err => {
-    //             res.json(err);
-    //         });
-    // });
+    app.get("/api/workouts/range", (req, res) => {
+        console.log("[API] Getting all entries from the database");
+        Workout.find({})
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
 
-    // app.get("/books", (req, res) => {
-    //     db.Book.find({})
-    //         .then(dbBook => {
-    //             res.json(dbBook);
-    //         })
-    //         .catch(err => {
-    //             res.json(err);
-    //         });
-    // });
+    app.post("/api/workouts", ({ body }, res) => {
+        console.log("[API] Creating new workout");
+        Workout.create(body)
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
 
-    // app.get("/library", (req, res) => {
-    //     db.Library.find({})
-    //         .then(dbLibrary => {
-    //             res.json(dbLibrary);
-    //         })
-    //         .catch(err => {
-    //             res.json(err);
-    //         });
-    // });
+    app.put("/api/workouts/:id", ({ body }, res) => {
+        console.log("[API] Creating new workout");
+        Workout.create(body)
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
 
     // app.get("/populated", (req, res) => {
     //     db.Library.find({})
